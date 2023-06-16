19b7ae44162ec2ab7e95a56de2a9e3932f0c2a0f
K3yNet <enzo_vel@hotmail.com>
2023-06-16 13:22:01 -0300

    bugFix

--- types.hpp
+++ types.hpp
@@ -48,21 +48,21 @@ struct Memory{
   void printAll(){
     cout << "Banco de registradores" << endl;
     for (int i = 0; i < 32; i++){
-      cout << i << " " << registerBank[i].to_ulong() << endl;
+      cout << i << " " << registerBank[i] << endl;
     }
     cout << endl;
 
     cout << "L1 de dados" << endl;
     for (int i = 0; i < (64 * 1024); i++){
       if(!L1DataCache[i].none()){
-        cout << i << " " << L1DataCache[i].to_ulong() << endl;
+        cout << i << " " << L1DataCache[i] << endl;
       }
     }
 
     cout << "L1 de instrucoes" << endl;
     for (int i = 0; i < (64 * 1024); i++){
       if(!L1InstructionCache[i].none()){
-        cout << i << " " << L1InstructionCache[i].to_ulong() << endl;
+        cout << i << " " << L1InstructionCache[i] << endl;
       }
     }
     
